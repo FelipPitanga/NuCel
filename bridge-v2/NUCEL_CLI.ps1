5ce0c79f962b569567267afa70fe78045b2e5ef5
dev back-end <103066721+FelipPitanga@users.noreply.github.com>
2026-09-22 12:08:49 -0300

    Do not open browser on NuCel server start

--- bridge-v2/NUCEL_CLI.ps1
+++ bridge-v2/NUCEL_CLI.ps1
@@ -85,7 +85,13 @@ switch ($action.ToLowerInvariant()) {
     }
 
     'open' {
-        Start-Process 'https://nucel.nuvixgestao.workers.dev/'
+        $url = Get-TunnelUrl
+        if ($url) {
+            $encoded = [Uri]::EscapeDataString($url)
+            Start-Process ('https://nucel.nuvixgestao.workers.dev/?bridgeUrl=' + $encoded)
+        } else {
+            Start-Process 'https://nucel.nuvixgestao.workers.dev/'
+        }
         exit 0
     }
 
@@ -104,7 +110,7 @@ switch ($action.ToLowerInvariant()) {
         Write-Host '[NuCel] Iniciando em segundo plano...' -ForegroundColor Cyan
         Write-Host '[NuCel] Atualizando e preparando ENGINE + TUNEL.' -ForegroundColor DarkGray
 
-        $cmdArgs = '/d /c ""' + $StartCmd + '" --hidden >> "' + $StartLog + '" 2>&1"'
+        $cmdArgs = '/d /c ""' + $StartCmd + '" --hidden --no-browser >> "' + $StartLog + '" 2>&1"'
         Start-Process -FilePath $env:ComSpec -ArgumentList $cmdArgs -WorkingDirectory $BridgeDir -WindowStyle Hidden | Out-Null
 
         $deadline = (Get-Date).AddMinutes(4)
